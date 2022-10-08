f3addb699f5456dfb995af41874ecf02ad85b144
Christoph Rinne <crinne@ufg.uni-kiel.de>
2022-10-08 18:58:32 +0200

    solved problem bbox coord-order, reorganized data

--- London_places.R
+++ London_places.R
@@ -1,15 +1,28 @@
 # still unfinished, for plotting sites in London
+london_data <- rbind.data.frame(
+    c("St. Marylebone", 51.5225,-0.152222),
+    c("St. Mary Graces",51.509289,-0.072916 ),
+    c("Merton Priory",51.414517,-0.181272),
+    c("St. Benet",51.513194,-0.091389),
+    c("St. Bride",51.513889,-0.100278),
+    c("Bermondesy Abbey", 51.4975,-0.080833)
+  ) 
+colnames(london_data) <-c("name", "lat", "lon")
+london_data$lat<-as.numeric(london_data$lat)
+london_data$lon<-as.numeric(london_data$lon)
 
-london_names <- c("St. Marylebone", "St. Mary Graces", "Merton Priory", "St. Benet", "St. Bride", "Bermondesy Abbey")
-latitude <- c(51.5225, 51.509289, 51.414517, 51.513194, 51.513889, 51.4975)
-longitude <- c(-0.152222, -0.072916, -0.181272, -0.091389, -0.100278, -0.080833)
-london_data <- data.frame(london_names, latitude, longitude)
-london_boundary_lat_max <- max(london_data$latitude) + 0.1
-london_boundary_lat_min <- min(london_data$latitude) - 0.1
-london_boundary_long_max <- max(london_data$longitude) + 0.1
-london_boundary_long_min <- min(london_data$longitude) - 0.1
-bbox <- get_bbox (c (london_boundary_long_min, london_boundary_lat_min, 
-                     london_boundary_long_max, london_boundary_lat_max))
-dat_B <- extract_osm_objects (key = "building", bbox = bbox)
+bbox <- get_bbox(
+  c(
+    min(london_data$lon) + 0.1,
+    min(london_data$lat) + 0.1,
+    max(london_data$lon) - 0.1,
+    max(london_data$lat) - 0.1
+  )
+)
+
+dat_Res <- extract_osm_objects (key = "landuse", value = "residential", geom_only = TRUE, bbox = bbox)
+
 map <- osm_basemap (bbox = bbox, bg = "gray20")
-map <- add_osm_objects (map, dat_B, col = "gray40")
+map <- add_osm_objects (map, dat_Res, col = "gray40")
+plot(map)
+
