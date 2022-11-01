5e8be4b6ebeec2ff25f146cafdd7d3872d45f77c
crinne <crinne@ufg.uni-kiel.de>
2022-11-01 11:12:58 +0100

    code reviewd

--- lifetables_processing/coale_demeny_life_tables_gompertz.R
+++ lifetables_processing/coale_demeny_life_tables_gompertz.R
@@ -1,10 +1,11 @@
+# Calculate Coale-Demeny regional model life tables for comparison
+
 sex <- c("F", "M")
-# x_vec <- demogR::cdmltw("F")$age[-c(1,2, 3, 4)] - 15
 x_vec <- seq(from = 0, to = 80, by = 5)
-# x_vec2 <- demogR::cdmltw("F")$age[-c(1,2, 3, 4)] + 5 - 15
 x_vec2 <- seq(from = 5, to = 85, by = 5)
+
 gompertz_df <- data.frame(i = NA, Gompertz_shape = NA, Gompertz_rate = NA, mx_15 = NA)
-#Frage: Warum die vier Himmelsrichtungen?
+
 for (s in sex) {
   lt_cdmltw <- demogR::cdmltw(s)
   lt_cdmlte <- demogR::cdmlte(s)
@@ -13,14 +14,9 @@ for (s in sex) {
   lt_cd_all <- list(lt_cdmltw = lt_cdmltw, lt_cdmlte = lt_cdmlte, lt_cdmlts = lt_cdmlts, lt_cdmltn = lt_cdmltn)
   for (t in 1:length(lt_cd_all)) {
     for (v in 1:25) {
-      dx_vec <- round(lt_cd_all[[t]]$ndx[v,-(1:4)] * 100000)#[v,]
+      dx_vec <- round(lt_cd_all[[t]]$ndx[v,-(1:4)] * 100000)
       mx_15 <- lt_cd_all[[t]]$nmx[v,5]
-      # das folgende passt nicht wegen unterschiedlicher Vektorlänge
       mort_df <- data.frame(x_vec, x_vec2, dx_vec)
-      # C_D_Gomp <-  flexsurv::flexsurvreg(formula = survival::Surv(x_vec) ~ 1,
-      #                                    weights = dx_vec, data = mort_df, dist="gompertz")
-      # Gompertz_shape <- C_D_Gomp$coefficients[1]
-      # Gompertz_rate <- exp(C_D_Gomp$coefficients[2])
       
       MLE_lt <- Gomp.MLE.interval(mort_df, agebegin = "x_vec", ageend = "x_vec2", Dx = "dx_vec")
       Gompertz_shape <- MLE_lt[2]
