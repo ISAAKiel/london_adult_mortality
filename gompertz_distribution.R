a4f6d7a6bc1bca88af7f9b0a2186617d2b94dbcf
Christoph Rinne <crinne@ufg.uni-kiel.de>
2022-12-04 15:53:28 +0100

    moved legend to 1. plot last changes saved

--- gompertz_distribution.R
+++ gompertz_distribution.R
@@ -28,14 +28,10 @@ gridExtra::grid.arrange (
     xlab("age in years")  + ylab("density"),
   
   ggplot() + xlim(15, 105) + ylim(0, 1) +
-    geom_function(fun = function(x) gomp_lx(x - 15, exp((-66.77) * (0.025 - 0.0718) - 7.119), 0.025),
-                  aes(col = "\u03B2 = 0.025")) +
-    geom_function(fun = function(x) gomp_lx(x - 15,  exp((-66.77) * (0.04 - 0.0718) - 7.119), 0.04), 
-                  aes(col = "\u03B2 = 0.04")) +
-    geom_function(fun = function(x) gomp_lx(x - 15,  exp((-66.77) * (0.06 - 0.0718) - 7.119), 0.06), 
-                  aes(col = "\u03B2 = 0.06")) +
-    geom_function(fun = function(x) gomp_lx(x - 15,  exp((-66.77) * (0.09 - 0.0718) - 7.119), 0.09), 
-                  aes(col = "\u03B2 = 0.09")) +
+    geom_function(fun = function(x) gomp_lx(x - 15, exp((-66.77) * (0.025 - 0.0718) - 7.119), 0.025), colour = "red") +
+    geom_function(fun = function(x) gomp_lx(x - 15,  exp((-66.77) * (0.04 - 0.0718) - 7.119), 0.04), colour = "blue") +
+    geom_function(fun = function(x) gomp_lx(x - 15,  exp((-66.77) * (0.06 - 0.0718) - 7.119), 0.06), colour = "green") +
+    geom_function(fun = function(x) gomp_lx(x - 15,  exp((-66.77) * (0.09 - 0.0718) - 7.119), 0.09), colour = "dark grey") +
     ylab("survival") + xlab("age in years"),
   
   ncol = 2
