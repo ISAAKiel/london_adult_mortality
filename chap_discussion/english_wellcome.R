7c3b1d796373b50474b961b1ecfd8eaea23c2bb4
crinne <crinne@ufg.uni-kiel.de>
2023-02-02 12:21:09 +0100

    x_scale für 200-Jahre-Intervall, uberflüssiger Code am Ende

--- chap_discussion/english_wellcome.R
+++ chap_discussion/english_wellcome.R
@@ -15,6 +15,7 @@ english_wellcome_plot <- ggplotGrob(
     geom_point(aes(x = as.numeric(substr(year, 2, 5)), y = M), size= 3 ) + 
     geom_point(aes(x = (start + end) / 2, y = M), size= 3) +
     guides(size = "none",colour=guide_legend(ncol=1)) +
+    scale_x_continuous (breaks = seq(1200, 1800, by = 200)) +
     theme(legend.position="none")
   )
 
@@ -35,20 +36,22 @@ english_wellcome_plot_r <- ggplotGrob(
     geom_point(aes(x = as.numeric(substr(year, 2, 5)), y = M), size= 3 )+ 
     geom_point(aes(x = (start + end) / 2, y = M), size= 3) + 
     guides(size = "none",colour=guide_legend(ncol=1)) +
+    scale_x_continuous (breaks = seq(1200, 1800, by = 200)) +
     theme(legend.position="none")
   )
 ewp_legend <- get_legend(english_wellcome_plot)
 
+# Nicht genutzter Code?
 # Gompertz beta from historical and osteological data
-english_wellcome_beta <- rbind(english_mortality_beta_prep, wellcome_prep_beta)
-english_wellcome_beta$data <- factor(english_wellcome_beta$data, levels = unique(english_wellcome_beta$data))
-english_wellcome_beta$start <- as.numeric(english_wellcome_beta$start) 
-english_wellcome_beta$end <- as.numeric(english_wellcome_beta$end) 
+#english_wellcome_beta <- rbind(english_mortality_beta_prep, wellcome_prep_beta)
+#english_wellcome_beta$data <- factor(english_wellcome_beta$data, levels = unique(english_wellcome_beta$data))
+#english_wellcome_beta$start <- as.numeric(english_wellcome_beta$start) 
+#english_wellcome_beta$end <- as.numeric(english_wellcome_beta$end) 
 
-english_wellcome_plot_beta <- 
-  ggplot(english_wellcome_beta, aes(colour = data, shape = source) ) +  
-  ylab("Gompertz \u03B2")  + xlab("year") + #ylim(10, 70) +
-  geom_errorbar(aes(x = (start + end) / 2, y = beta, ymin = HDIlow, ymax=  HDIhigh), width=0, colour = "dark grey") +
-  geom_errorbarh(aes(x = (start + end) / 2, y = beta, xmax = start, xmin = end, height = 0), colour = "dark grey") +
-  geom_point(aes(x = as.numeric(substr(year, 2, 5)), y = beta), size= 3 )+ 
-  geom_point(aes(x = (start + end) / 2, y = beta), size= 3) + guides(size = "none")
+#english_wellcome_plot_beta <- 
+#  ggplot(english_wellcome_beta, aes(colour = data, shape = source) ) +  
+#  ylab("Gompertz \u03B2")  + xlab("year") + #ylim(10, 70) +
+#  geom_errorbar(aes(x = (start + end) / 2, y = beta, ymin = HDIlow, ymax=  HDIhigh), width=0, colour = "dark grey") +
+#  geom_errorbarh(aes(x = (start + end) / 2, y = beta, xmax = start, xmin = end, height = 0), colour = "dark grey") +
+#  geom_point(aes(x = as.numeric(substr(year, 2, 5)), y = beta), size= 3 )+ 
+#  geom_point(aes(x = (start + end) / 2, y = beta), size= 3) + guides(size = "none")
