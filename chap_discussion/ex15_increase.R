151059b573da6ef52225286adf18b3f04b4dcbba
nmueller18 <mueller-scheessel@gmx.net>
2023-02-22 00:30:09 +0100

    Re-Calculation for share in population increase

--- chap_discussion/ex15_increase.R
+++ chap_discussion/ex15_increase.R
@@ -1,5 +1,5 @@
 # London population increase between 1650 and 1841 (Greater London!)
-london_increase <- 2207653 / 375000
+london_increase <- (2207653 - 375000) / 375000
 
 # in the 1841 census, 1,293,969 Inner Londoners (not Greater London) were of age 15 or above, 
 # while 576,449 were below this threshold
@@ -12,8 +12,9 @@ age15_perc <- 1293969 / (576449 + 1293969)
 new_churchyard_a15 <- 0.021205430 * exp(0.0401 * 3)
 ex_london1841 <- gomp.ex(15, 0.004512645, 0.054650697, age_start = 15)
 ex_new_churchyard <- gomp.ex(15, new_churchyard_a15, 0.0401, age_start = 15)
-ex_rel <- ex_london1841 / (ex_new_churchyard)
+ex_rel <- (ex_london1841 - ex_new_churchyard) / ex_new_churchyard
 
 # share of later adult mortality in population increase
 ex_increase <- ex_rel * age15_perc
 mort_explain <- ex_increase / london_increase 
+mort_explain_share <- 1 / mort_explain
