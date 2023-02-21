6792b2357d78a600250082cf0fb6e53b14d74a8a
crinne <crinne@ufg.uni-kiel.de>
2023-02-21 13:22:04 +0100

    factor levels order adjusted

--- chap_discussion/english_wellcome.R
+++ chap_discussion/english_wellcome.R
@@ -1,6 +1,6 @@
 # get symbols & colors from palette alphabet (max n = 26), alt. glasbey (32), polychrome(36)
 plotcolors<-palette.colors(palette = 'alphabet')
-plotsymbols<-c(15,17,18)
+plotsymbols<-c(17,18,15)
 
 # MOLA Welcome data without correction of population growth
 english_wellcome <- rbind(english_mortality_prep, wellcome_prep)
@@ -10,7 +10,9 @@ english_wellcome <- english_wellcome %>%
   mutate(data = factor(data, levels = unique(data))) %>%
   mutate(source = gsub('written','England & Wales written', source)) %>%
   mutate(source = gsub('osteological','London osteological', source)) %>%
-  mutate_if(source = case_when(substr(data,1,6)=="London" ~ "London written")) %>%
+  mutate(source = ifelse(substr(data,1,6)=="London","London written",source)) %>%
+  mutate(source = factor(source, levels = c('England & Wales written', 'London written', 
+                         'London osteological'))) %>%
   mutate(start = as.numeric(start)) %>%
   mutate(end = as.numeric(end)) %>%
   mutate(year = ifelse(is.na(year), (start + end)/2, substr(year, 2,5))) %>%
@@ -24,8 +26,7 @@ ggplot(english_wellcome, aes(x = year, y = M, colour = data, shape = source) ) +
   geom_smooth(color = "dark grey") +
   geom_errorbar(aes(ymin = HDIlow, ymax=  HDIhigh), width=0, colour = "dark grey") +
   geom_errorbarh(aes(xmax = start, xmin = end, height = 1)) +
-  geom_point(size= 3 ) + 
-  geom_point(size= 3) +
+  geom_point(size= 2 ) + 
   guides(size = "none",colour=guide_legend(ncol=1)) +
   scale_x_continuous (breaks = seq(1200, 1800, by = 200)) +
   theme(legend.position="none") -> english_wellcome_plot
@@ -36,6 +37,11 @@ english_wellcome_r <- rbind(english_mortality_prep_r, wellcome_prep_r)
 # slight modifications
 english_wellcome_r <- english_wellcome_r %>%
   mutate(data = factor(data, levels = unique(data))) %>%
+  mutate(source = gsub('written','England & Wales written', source)) %>%
+  mutate(source = gsub('osteological','London osteological', source)) %>%
+  mutate(source = ifelse(substr(data,1,6)=="London","London written",source)) %>%
+  mutate(source = factor(source, levels = c('England & Wales written', 'London written', 
+                         'London osteological'))) %>%
   mutate(start = as.numeric(start)) %>%
   mutate(end = as.numeric(end)) %>%
   mutate(year = ifelse(is.na(year), (start + end)/2, substr(year, 2,5))) %>%
@@ -49,8 +55,7 @@ ggplot(english_wellcome_r, aes(x = year, y = M, colour = data, shape = source) )
   geom_smooth(color = "dark grey") +
   geom_errorbar(aes(ymin = HDIlow, ymax=  HDIhigh), width=0, colour = "dark grey") +
   geom_errorbarh(aes(xmax = start, xmin = end, height = 1)) +
-  geom_point(size= 3 )+ 
-  geom_point(size= 3) + 
+  geom_point(size= 2 )+ 
   guides(size = "none",colour=guide_legend(ncol=1)) +
   scale_x_continuous (breaks = seq(1200, 1800, by = 200)) -> english_wellcome_plot_r
 
