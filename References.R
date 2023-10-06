6d2d1a436e5ebf6b90b2ac10638676fb0ddcc8cf
nmueller18 <mueller-scheessel@gmx.net>
2023-10-06 22:10:57 +0200

    Some amendments, first reference.

--- References.R
+++
@@ -1,6 +0,0 @@
-# References
-
-
-# A. Kessler, Die Bevölkerung der Stadt Radolfzell am Bodensee im 17. und 18. Jahrhundert: demographische Strukturen einer „Ackerbürgerstadt“ vor Beginn der Industrialisierung. Hegau-Bibliothek 87 (Konstanz 1992).
-# U. Wendler, Nicht nur Pest und Pocken: zur Bevölkerungsgeschichte der Lüneburger Heide, des Wendlandes und der Marschen des Fürstentums Lüneburg 1550–1850. Quellen und Darstellungen zur Geschichte Niedersachsens 128 = Schriften des Freilichtmuseums am Kiekeberg 64 (Hannover 2008).
-# J. P. Süßmilch, Die göttliche Ordnung in den Veränderungen des menschlichen Geschlechts, aus der Geburt, dem Tode und der Fortpflanzung desselben (Berlin 1765).
