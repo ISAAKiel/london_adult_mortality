f0f8e59a64f7d30c0b06f5243220357a4e2fcc13
nmueller18 <mueller-scheessel@gmx.net>
2023-10-22 16:33:47 +0200

    Delete superfluous files and lines of code

--- chapter_supplement/simulations_run.R
+++ chapter_supplement/simulations_run.R
@@ -3,8 +3,6 @@ if (runCodeNew){
   lt_sim <- lt.MC(sampling = 1000,
                   n_min = 50,
                   n_max = 500,
-                  #M_min = 25,
-                  #M_max = 80,
                   b_min = 0.02,
                   b_max = 0.1,
                   error_range = NULL,
@@ -21,13 +19,11 @@ load(file.path(".", saveFileDir, "lt_sim.Rda") )
 
 lt_sim_shapes <- c("OLS_Gompertz_shape", "WOLS_Gompertz_shape", "NLS_Gompertz_shape",
                    "surv_Gompertz_shape", "surv_lt_Gompertz_shape", "surv_lt10_Gompertz_shape",
-                   #"MLE_Gompertz_shape", 
                    "MLE_adapted_Gompertz_shape", "MLE_lt_Gompertz_shape", "MLE_lt10_Gompertz_shape",
                    "bayes_gomp_b", "bayes_anthr_gomp_5y_b", "bayes_anthr_gomp_10y_b",
                    "bayes_poisson_b")
 lt_sim_shapes_names <- c("OLS", "WOLS", "WNLS",  
                          "survival", "survival (5y-cat)", "survival (10y-cat)",
-                         # "MLE", 
                          "MLE", "MLE (5y-cat)", "MLE (10y-cat)",
                          "Bayes", "Bayes (5y-cat)", "Bayes (10y-cat)", "Bayes poisson")
 lt_sim_estim_shapes <- c("OLS_estim_Gompertz_shape", "WOLS_estim_Gompertz_shape","NLS_estim_Gompertz_shape",  
@@ -95,7 +91,6 @@ MLE_wo_OL_df <- data.frame("MLE_wo_OL", MLE_wo_OL, MLE_wo_OL_count)
 colnames(MLE_wo_OL_df) <- c("method", "RMSE", "NAs")
 rmse_estim_result <- rbind(rmse_estim_result,MLE_wo_OL_df)
 
-
 plot_list_bayes_diff <- list(
   ggplot(lt_sim, aes(x = b_, y = b_ - bayes_anthr_gomp_b )) + geom_point(shape = 21) +
     xlab("original \u03B2") + ylab("original \u03B2 - estimated \u03B2") + 
@@ -104,75 +99,3 @@ plot_list_bayes_diff <- list(
     xlab("sample size") + ylab("original \u03B2 - estimated \u03B2") + 
     geom_smooth(method = "loess", formula = y ~ x)
 )
-
-ggplot(lt_sim) + 
-  geom_boxplot(aes(x = cut_interval(b_, length = 0.01),  y = surv_Gompertz_shape)) +
-  scale_y_continuous(breaks = round(seq(min(lt_sim$surv_Gompertz_shape), 
-                                        max(lt_sim$surv_Gompertz_shape), 
-                                        by = 0.01),2))
-ggplot(lt_sim) + 
-  geom_boxplot(aes(x = cut_interval(b_, length = 0.01),  y = MLE_lt_Gompertz_shape)) +
-  scale_y_continuous(breaks = round(seq(min(lt_sim$MLE_lt_Gompertz_shape), 
-                                        max(lt_sim$MLE_lt_Gompertz_shape), 
-                                        by = 0.01),2))
-ggplot(lt_sim) + 
-  geom_boxplot(aes(x = cut_interval(b_, length = 0.01),  y = bayes_gomp_b)) +
-  scale_y_continuous(breaks = round(seq(min(lt_sim$bayes_gomp_b), 
-                                        max(lt_sim$bayes_gomp_b), 
-                                        by = 0.01),2))
-ggplot(lt_sim) + 
-  geom_boxplot(aes(x = cut_interval(b_, length = 0.01),  y = bayes_anthr_gomp_b)) +
-  scale_y_continuous(breaks = round(seq(min(lt_sim$bayes_anthr_gomp_b), 
-                                        max(lt_sim$bayes_anthr_gomp_b), 
-                                        by = 0.01),2)) +
-  ylim(0.02,0.1)
-
-# this one gives no result: bayes_gomp_b not found
-ggplot(subset(lt_sim, bayes_gomp_a > 0.25)) + 
-  geom_point(aes(x = b_, y = bayes_gomp_b))
-
-ggplot(lt_sim) + 
-  geom_point(aes(x = 1/b_ * log(b_/a_),  
-                 y = 1 /  bayes_gomp_b* log(bayes_gomp_b / bayes_gomp_a))) +
-  xlim(-10,80) + ylim(-10,80)
-ggplot(lt_sim) + 
-  geom_point(aes(x = 1/b_ * log(b_/a_),  
-                 y = 1/b_ * log(b_/a_) - 1 /  bayes_gomp_b* log(bayes_gomp_b / bayes_gomp_a)))
-
-Metrics::rmse(1/lt_sim$b_ * log(lt_sim$b_/lt_sim$a_), 1 /  lt_sim$bayes_gomp_b* log(lt_sim$bayes_gomp_b / lt_sim$bayes_gomp_a))
-
-ggplot(lt_sim, aes(x = 1/b_ * log(b_/a_),  
-                   y = 1 /  bayes_anthr_gomp_b* log(bayes_anthr_gomp_b / bayes_anthr_gomp_a))) + 
-  geom_point() +
-  geom_smooth(method = "loess", formula = y ~ x) +
-  xlim(0,80) + ylim(0,80)
-
-ggplot(lt_sim, aes(x = b_, y = bayes_anthr_gomp_b )) + 
-  geom_point(shape = 21) +
-  geom_smooth(method = "loess", formula = y ~ x) + 
-  xlim(0.02,0.105) + ylim(0.02, 0.105) +
-  xlab("original \u03B2") + ylab("original \u03B2 - estimated \u03B2")
-
-ggplot(lt_sim, aes(x = log(a_), y = log(bayes_anthr_gomp_a ))) + 
-  geom_point(shape = 21) +
-  geom_smooth(method = "loess", formula = y ~ x) + 
-  xlim(-10,-3) + ylim(-11,-3) +
-  xlab("original \u03B2") + ylab("original \u03B2 - estimated \u03B2")
-  
-ggplot(lt_sim, aes(x = b_, y = bayes_anthr_gomp_10y_b )) + 
-  geom_point(shape = 21) +
-  geom_smooth(method = "loess", formula = y ~ x) + 
-  xlim(0.02,0.1) + ylim(0.02, 0.1) +
-  xlab("original \u03B2") + ylab("original \u03B2 - estimated \u03B2")
-
-ggplot(lt_sim, aes(x = b_, y = bayes_anthr_gomp_5y_b )) + 
-  geom_point(shape = 21) +
-  geom_smooth(method = "loess", formula = y ~ x) + 
-  xlim(0.02,0.1) + ylim(0.02, 0.1) +
-  xlab("original \u03B2") + ylab("original \u03B2 - estimated \u03B2") 
-
-ggplot(lt_sim, aes(x = b_, y = bayes_gomp_b )) + 
-  geom_point(shape = 21) +
-  geom_smooth(method = "loess", formula = y ~ x) + 
-  xlim(0.02,0.1) + ylim(0.02, 0.1) +
-  xlab("original \u03B2") + ylab("original \u03B2 - estimated \u03B2")
